a24a7628ddb01ec458601dc5f6099daab47f7d72
Vishal Agarwala <agarwala@me.com>
2021-05-28 15:51:32 -0400

    added morgan

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const apiRouter = require('./routes');
 
 let app = express();
@@ -8,6 +9,7 @@ let app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname, '../client')));
 app.use('/api', apiRouter);
